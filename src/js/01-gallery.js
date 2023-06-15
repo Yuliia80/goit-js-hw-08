e0d7f9a784927f5fc0d5548b6095aa8c79e2e26f
Yuliia80 <tretyak_1309@ukr.net>
2023-06-15 23:20:45 +0300

    hw8 (1)

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const galleryEl = document.querySelector('.gallery');
+const markup = galleryItems.map(({ preview, original, description }) => 
+    `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}"/>
+    </a>
+  </li>`
+);
+
+galleryEl.insertAdjacentHTML('beforeend', markup.join(''));
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt', 
+  captionPosition: 'bottom', 
+  captionDelay: 250
+});
